98bb566d15350cf5c5a99b26a8b266dcaf340015
OkorehMercy <okorehmercy@gmail.com>
2017-06-05 05:22:33 -0700

    Update

--- sysAdmin/AdminApp.java
+++ sysAdmin/AdminApp.java
@@ -1,11 +1,16 @@
 
 package sysadmin;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
 import javafx.application.Application;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import sysadmin.net.Connector;
+import sysadmin.net.workers.Receiver;
+import sysadmin.net.workers.Sender;
 import sysadmin.ui.UIManager;
 
 
@@ -17,14 +22,29 @@ public class AdminApp extends Application
         Connector conn = new Connector();
         UIManager uiManager;
         
-        if(!conn.connect()){
-            Alert alrt = new Alert(Alert.AlertType.ERROR);
-            alrt.setTitle("-- Error --");
-            alrt.setHeaderText("Network error");
-            alrt.setContentText("Could not connect to server");
-            alrt.showAndWait();
-        }
-        else{
+        //worker threads
+        Sender sndr;
+        Receiver rcvr;
+        OutputStream out = null;
+        InputStream in = null;
+        
+        if(conn.connect()){
+            setUpStreams();
+            //start threads
+            out = conn.getOutputStream();
+            in = conn.getInputStream();
+            
+            if(out != null){
+                //start sender
+                sndr = new Sender(out);
+                sndr.start();
+            }
+            
+            if(in != null){
+                //start receiver
+                rcvr = new Receiver(in);
+                rcvr.start();
+            }
             //create ui manager
             uiManager = new UIManager(primaryStage);
             //set up scenes
@@ -32,10 +52,34 @@ public class AdminApp extends Application
             //show
             uiManager.setVisible();
         }
-    }
-
-    public static void main(String[] args) {
-        launch(args);
+        else{
+            Alert alrt = new Alert(Alert.AlertType.ERROR);
+            alrt.setTitle("-- Error --");
+            alrt.setHeaderText("Network error");
+            alrt.setContentText("Could not connect to server");
+            alrt.showAndWait();
+        }
     }
     
+    
+    //create and add piped out put streams to global map
+    private static void setUpStreams()
+    {
+        //chat worker out put stream
+        UIManager.addStream("chat", new PipedOutputStream());
+        //general application out put stream, will be used
+        //by the Receiver worker to forward log in confirmation data received
+        //from the server
+        UIManager.addStream("app", new PipedOutputStream());
+        //events worker out put stream
+        //UIManager.addStream("events", new PipedOutputStream());
+        //Sender worker out put stream
+        UIManager.addStream("sender", new PipedOutputStream());
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
 }
