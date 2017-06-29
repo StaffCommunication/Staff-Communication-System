3f5c10ab2fe4d5c1f986a0bc8d148f69da6fa299
Vincent Mwendwa <vrubben@outlook.com>
2017-06-28 23:31:33 -0700

    Update and rename ExploreButton.java to Explore.java

--- sysAdmin/ui/menu/Explore.java
+++ sysAdmin/ui/menu/Explore.java
@@ -1,22 +1,33 @@
 
 package sysadmin.ui.menu;
 
+
+import java.io.PipedInputStream;
 import javafx.scene.control.ToggleButton;
+import sysadmin.ui.menu.explore.ExploreWorker;
+
 
 /*
 ** to navigate the entire list of registered staff members,
 ** this toggle button will take you there
 */
 
-public class ExploreButton extends ToggleButton
+public class Explore extends ToggleButton
 {
-    
+    private ExploreWorker xPane;
     //define constructor
-    public ExploreButton()
+    public Explore()
     {
+        xPane = new ExploreWorker(new PipedInputStream());
+        xPane.setUp();
         //set button name
         setText("Explore");
         //setAlignment(Pos.CENTER);
         getStyleClass().add("menu-toggle");
+    }
+    
+    public ExploreWorker getExplorePane()
+    {
+        return xPane;
     }
 }
