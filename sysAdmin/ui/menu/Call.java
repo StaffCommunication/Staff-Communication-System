925800ad878b2fb5e39f16a109cc521e106bbedb
Vincent Mwendwa <vrubben@outlook.com>
2017-06-28 23:29:14 -0700

    Update and rename AudioCallButton.java to Call.java

--- sysAdmin/ui/menu/Call.java
+++ sysAdmin/ui/menu/Call.java
@@ -3,21 +3,29 @@ package sysadmin.ui.menu;
 
 
 import javafx.scene.control.ToggleButton;
+import sysadmin.ui.menu.call.CallerWorker;
 
 /*
 ** start an audio call, view calls log and view a list off all contacts
 */
 
-public class AudioCallButton extends ToggleButton
+public class Call extends ToggleButton
 {
     
+    private CallerWorker cw = new CallerWorker();
     
     //define constructor
-    public AudioCallButton()
+    public Call()
     {
+        //cw.setUp();
         //set button name
-        setText("Audio Call");
+        setText("Call");
         //setAlignment(Pos.CENTER);
         getStyleClass().add("menu-toggle");
+    }
+    
+    public CallerWorker getCallerPane()
+    {
+        return cw;
     }
 }
