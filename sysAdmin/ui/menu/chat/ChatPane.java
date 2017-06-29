b5497b1b1be5e2d75f4e30867abaa053f20ac391
Vincent Mwendwa <vrubben@outlook.com>
2017-06-28 23:38:17 -0700

    Create ChatPane.java

---
+++ sysAdmin/ui/menu/chat/ChatPane.java
@@ -0,0 +1,66 @@
+
+package sysadmin.ui.menu.chat;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import sysadmin.net.msg.Message;
+
+/*
+** a border pane to hold the ChatArea, header and footer (TextMsgArea)
+** 
+*/
+
+public class ChatPane extends BorderPane
+{
+    //header
+    private static Label hdr;
+    //destination od a message
+    private static String dest;
+    //chat messages area
+    private TextMsgArea tMsg;
+
+    //constructor
+    public ChatPane(TextMsgArea tm)
+    {
+        ChatPane.hdr = new Label();
+        tMsg = tm;
+        setTop(hdr);
+        setBottom(tMsg);
+        
+        tMsg.setup();
+    }
+    
+    //change chat area
+    public void setChatArea(ChatArea ca)
+    {
+        setCenter(ca);
+    }
+    
+    public static void setHeader(String hder)
+    {
+        hdr.setText(hder);
+    }
+    
+    //get message
+    public Message getMsg()
+    {
+        return tMsg.getMsg();
+    }
+    
+    //set source
+    public static void setDest(String d)
+    {
+        dest = d;
+    }
+    
+    //get the header, this is the name
+    public static String getDest()
+    {
+        return dest;
+    }
+    
+    public void clear()
+    {
+        tMsg.clear();
+    }
+}
